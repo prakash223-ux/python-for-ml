0b2f9df1b1e6f92a2e22d00a7826ef690232855c
prakash223-ux <prakashchlo64@gmail.com>
2026-02-04 17:45:32 +0530

    Add Day 1 python practice program

---
+++ Day-01-Python-Basics/day1.py
@@ -0,0 +1,69 @@
+# 1. Print name
+print("Muthu Prakash")
+
+# 2. Add two numbers
+a, b = 10, 5
+print(a + b)
+
+# 3. Subtract
+print(a - b)
+
+# 4. Multiply
+print(a * b)
+
+# 5. Divide
+print(a / b)
+
+# 6. Input name
+name = "developer"
+print(name)
+
+# 7. String to int
+x = "25"
+print(int(x))
+
+# 8. Int to float
+print(float(10))
+
+# 9. Even or odd
+num = 4
+print("Even" if num % 2 == 0 else "Odd")
+
+# 10. Compare numbers
+print(10 > 5)
+
+# 11. Area of circle
+r = 5
+print(3.14 * r * r)
+
+# 12. Simple interest
+p, t, r = 1000, 2, 5
+print((p * t * r) / 100)
+
+# 13. Swap numbers
+a, b = 5, 10
+a, b = b, a
+print(a, b)
+
+# 14. Boolean comparison
+print(5 < 8)
+
+# 15. Type check
+print(type(10))
+
+# 16. String concat
+print("AI" + " " + "ML")
+
+# 17. String length
+print(len("Machine Learning"))
+
+# 18. Upper & lower
+print("Python".upper())
+print("Python".lower())
+
+# 19. Square
+n = 4
+print(n * n)
+
+# 20. Cube
+print(n * n * n)
